15f0c91466b1cae53ce3263476785f998892f12b
javierpelayo <contact@javierperez.dev>
2019-12-27 15:16:16 -0800

    Added back out of ten due to mixup with comments partaining to age and ratings.

--- app.py
+++ app.py
@@ -47,37 +47,37 @@ while ind != 10:
 
         ## check comment content if it has a rating, if it does append the rating
         for content in comment_content:
-            if "1/" in content:
+            if "1/10" in content:
                 rating.append(1)
-            elif "4.5" in content:
+            elif "4.5/10" in content:
                 rating.append(5)
-            elif "5.5" in content:
+            elif "5.5/10" in content:
                 rating.append(6)
-            elif "6.5" in content:
+            elif "6.5/10" in content:
                 rating.append(7)
-            elif "7.5" in content:
+            elif "7.5/10" in content:
                 rating.append(8)
-            elif "8.5" in content:
+            elif "8.5/10" in content:
                 rating.append(9)
-            elif "9.5" in content:
+            elif "9.5/10" in content:
                 rating.append(10)
-            elif "2" in content:
+            elif "2/10" in content:
                 rating.append(2)
-            elif "3" in content:
+            elif "3/10" in content:
                 rating.append(3)
-            elif "4" in content:
+            elif "4/10" in content:
                 rating.append(4)
-            elif "5" in content:
+            elif "5/10" in content:
                 rating.append(5)
-            elif "6" in content:
+            elif "6/10" in content:
                 rating.append(6)
-            elif "7" in content:
+            elif "7/10" in content:
                 rating.append(7)
-            elif "8" in content:
+            elif "8/10" in content:
                 rating.append(8)
-            elif "9" in content:
+            elif "9/10" in content:
                 rating.append(9)
-            elif "10/" in content:
+            elif "10/10" in content:
                 rating.append(10)
 
             ## If we already commented on this post
